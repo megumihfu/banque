0eb269b37d7cb90fbbcf86146577ff71a2d55836
Inès DAFFEF <daffef@et.esiea.fr>
2023-11-01 18:30:50 +0100

    Ajout d'un gitignore + test de CapManagement

--- core/src/main/java/Main.java
+++ core/src/main/java/Main.java
@@ -3,6 +3,7 @@ import datatransferobject.Request;
 import entities.BankAccount;
 import usecases.BankTransfer;
 import usecases.ConverterDevice;
+import usecases.CapManagement;
 import datatransferobject.Response;
 import datatransferobject.IRequestHandle;
 
@@ -35,6 +36,14 @@ public class Main {
         System.out.println("source acc = "+sourceAccount.getBalance());
         System.out.println("target acc = "+targetAccount.getBalance());
         System.out.println(response.getResponseData());*/
+
+        BankAccount sourceAccount = new BankAccount(accountId, 5000, 1500);
+        System.out.println("source acc = "+sourceAccount.getLimit());
+        Request capRequest = new Request("3500.0");
+        CapManagement capManagement = new CapManagement(reqHandle);
+        Response response = capManagement.manageCapital(capRequest, sourceAccount);
+        System.out.println("source acc = "+sourceAccount.getLimit());
+        System.out.println(response.getResponseData());
 
 
     }

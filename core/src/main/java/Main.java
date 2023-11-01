dfb3991fbb47a8d9cc75710ff9b58929a20aeac2
MEDDOUR Yan <meddour@et.esiea.fr>
2023-11-01 11:30:52 +0000

    Update Main.java

--- core/src/main/java/Main.java
+++ core/src/main/java/Main.java
@@ -1,13 +1,41 @@
 import datatransferobject.ImplementIRequestHandle;
+import datatransferobject.Request;
+import entities.BankAccount;
+import usecases.BankTransfer;
 import usecases.ConverterDevice;
 import datatransferobject.Response;
 import datatransferobject.IRequestHandle;
 
+import java.util.UUID;
+
 public class Main {
     public static void main(String[] args){
         IRequestHandle reqHandle = new ImplementIRequestHandle();
+        /* test ConverterDevice
+
         ConverterDevice converterDevice = new ConverterDevice(reqHandle);
         Response resp = converterDevice.runConversion();
-        System.out.println(resp.getResponseData());
+        System.out.println(resp.getResponseData());*/
+
+        UUID accountId = UUID.randomUUID();
+
+        /* test BankTransfer
+
+        BankAccount sourceAccount = new BankAccount(accountId, 5000, 1500);
+        BankAccount targetAccount = new BankAccount(accountId, 2000, 500);
+        System.out.println("source acc = "+sourceAccount.getBalance());
+        System.out.println("target acc = "+targetAccount.getBalance());
+
+        Request transferRequest = new Request("100.0");
+
+        BankTransfer bankTransfer = new BankTransfer(reqHandle);
+
+        Response response = bankTransfer.transferMoney(transferRequest, sourceAccount, targetAccount);
+
+        System.out.println("source acc = "+sourceAccount.getBalance());
+        System.out.println("target acc = "+targetAccount.getBalance());
+        System.out.println(response.getResponseData());*/
+
+
     }
 }

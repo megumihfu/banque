5606b8338dca0cf5cadd2d2fa5135cd80cfaa140
MEDDOUR Yan <meddour@et.esiea.fr>
2023-11-01 20:30:06 +0000

    Update ReceivedMoney.java

--- core/src/main/java/usecases/ReceivedMoney.java
+++ core/src/main/java/usecases/ReceivedMoney.java
@@ -1,4 +1,23 @@
 package usecases;
 
+import datatransferobject.Request;
+import datatransferobject.Response;
+import datatransferobject.IRequestHandle;
+import entities.BankAccount;
+
 public class ReceivedMoney {
+    private IRequestHandle requestHandle;
+
+    public ReceivedMoney(IRequestHandle requestHandle) {
+        this.requestHandle = requestHandle;
+    }
+
+    public Response receiveMoney(Request req, BankAccount targetAccount) {
+        double receivedAmount = Double.parseDouble(req.getReqData());
+
+        if (receivedAmount <= 0) {
+            return new Response("Invalid amount. Amount should be greater than 0.");
+        }
+        return new Response("Money received successfully. New balance: " + targetAccount.getBalance());
+    }
 }
